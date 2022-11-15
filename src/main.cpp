dd7371da620fd0feccdf63c47acdf177308e4645
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-11-15 16:42:15 +0000

    Testing code with Laser
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,22 @@
 // https://github.com/ppedro74/Arduino-SerialCommands
 #include <SerialCommands.h>
 
-#define LASER_OUT 2 // PD2  
-const int button_pin = 3; // PD3
+const int button_pin = 2; // PD2
+const int LASER_OUT = 3; // PD3
+
 const int laser_period = 80; // in us
 
 /*
    Red connector 
 
-   ----  ----
-   | 2   GND  |
-   ------------
+   ----  ------------------------------
+   | +   SIG (tie to GND to OFF laser  |
+   | -   GND                           |
+   ------------------------------------
 
 */
 
-bool pulse_led = false;
+bool pulse_laser = true;
 
 //unsigned long nextPtime = 0;
 char serial_command_buffer_[32];
@@ -29,26 +31,31 @@ void cmd_unrecognized(SerialCommands* sender, const char* cmd) {
     sender->GetSerial()->print(cmd);
     sender->GetSerial()->println("]");
 }
+
 //called for ON command
 void cmd_led_on(SerialCommands* sender) {
-    pulse_led = true;
-    digitalWrite(LED_BUILTIN, HIGH);
+    pulse_laser = true;
+    digitalWrite(LED_BUILTIN, pulse_laser);
     sender->GetSerial()->println("Pulse is on");
 }
 
 //called for OFF command
 void cmd_led_off(SerialCommands* sender) {
-    pulse_led = false;
+    pulse_laser = false;
     digitalWrite(LED_BUILTIN, LOW);
     sender->GetSerial()->println("Pulse is off");
 }
+
 //Note: Commands are case sensitive
 SerialCommand cmd_led_on_("ON", cmd_led_on);
 SerialCommand cmd_led_off_("OFF", cmd_led_off);
 //SerialCommand cmd_datetime_set_("TS", cmd_datetime_set); // requires one argument
 /// Add one_key commands
+SerialCommand cmd_ledo_("o", cmd_led_on, true);
 SerialCommand cmd_ledf_("f", cmd_led_off, true);
 
+unsigned long nextLtime, nextBtime;
+bool laser_state=true;
 
 //delay(1);
 void setup() {
@@ -57,38 +64,19 @@ void setup() {
     pinMode(LASER_OUT, INPUT_PULLUP);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
+    pinMode(button_pin, INPUT_PULLUP);
     // Start serial port
     Serial.begin(115200);
     serial_commands_.SetDefaultHandler(cmd_unrecognized);
     serial_commands_.AddCommand(&cmd_led_on_);
     serial_commands_.AddCommand(&cmd_led_off_);
     serial_commands_.AddCommand(&cmd_ledf_);
-//    serial_commands_.AddCommand(&cmd_datetime_set_);
+    serial_commands_.AddCommand(&cmd_ledo_);
 //    serial_commands_.AddCommand(&cmd_print_);
 
     Serial.println(F("Ola Mundo!"));
-}
-
-void laser_pulse(unsigned long now_us) {
-    static unsigned long nextLtime = 0;
-    static bool led_state=true;
-    if (pulse_led){
-
-        if ( now_us > nextLtime ) {
-            if(led_state){
-                pinMode(LASER_OUT, INPUT_PULLUP); // laser ON
-                nextLtime += laser_period / 2;
-
-            }
-            else {
-                pinMode(LASER_OUT, OUTPUT);
-                digitalWrite(LASER_OUT, LOW);
-                nextLtime += laser_period / 2;
-            }
-        }
-    }
-    else
-        pinMode(LASER_OUT, INPUT_PULLUP);
+    nextLtime = 0;
+    nextBtime = 0;
 }
 
 void laser_off() {
@@ -96,8 +84,41 @@ void laser_off() {
     digitalWrite(LASER_OUT, LOW);
 }
 
+void laser_pulse(unsigned long now_us) {
+    if (pulse_laser){
+
+        if ( now_us > nextLtime ) {
+            if(laser_state){
+                pinMode(LASER_OUT, INPUT_PULLUP); // laser ON
+            }
+            else {
+                laser_off();
+            }
+            nextLtime += laser_period / 2;
+            laser_state = not laser_state;  // Toggle
+        }
+    }
+    else {
+        //pinMode(LASER_OUT, INPUT_PULLUP);
+        laser_state=false;
+        laser_off();
+    }
+}
+
+void read_button(unsigned long now_ms) {
+        if ( now_ms > nextBtime ) {
+            if (!digitalRead(button_pin)){
+                pulse_laser = not pulse_laser;
+                digitalWrite(LED_BUILTIN, pulse_laser);
+                nextBtime += 500;  // in millis
+            }
+        }
+
+}
 void loop() {
     unsigned long us = micros();
     laser_pulse(us);
+    unsigned long ms = millis();
+    read_button(ms);
     serial_commands_.ReadSerial();
 }
