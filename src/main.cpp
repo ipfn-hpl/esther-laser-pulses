1268fa9140024633c5c94203d7ed19e5a5945fd8
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-12-20 18:43:07 +0000

    added RGB Leds
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 
 const int button_pin = 2; // PD2
 const int LASER_OUT = 3; // PD3
+// -- Only A0-A5 can be Digital I/O
+const int RED_LED = A5; // A7;
+const int GREEN_LED = A4;
+const int BLUE_LED = A3; // A7;
 
 const int laser_period = 80; // in us
 
@@ -20,6 +24,7 @@ const int laser_period = 80; // in us
 */
 
 bool pulse_laser = true;
+bool redOn = false, greenOn = false, blueOn = false;
 
 //unsigned long nextPtime = 0;
 char serial_command_buffer_[32];
@@ -32,6 +37,29 @@ void cmd_unrecognized(SerialCommands* sender, const char* cmd) {
     sender->GetSerial()->println("]");
 }
 
+void cmd_green_on(SerialCommands* sender) {
+    greenOn = true;
+    sender->GetSerial()->println("green led is on");
+}
+void cmd_green_off(SerialCommands* sender) {
+    greenOn = false;
+    sender->GetSerial()->println("green led is off");
+}void cmd_blue_on(SerialCommands* sender) {
+    blueOn = true;
+    sender->GetSerial()->println("Blue led is on");
+}
+void cmd_blue_off(SerialCommands* sender) {
+    blueOn = false;
+    sender->GetSerial()->println("Blue led is off");
+}
+void cmd_red_on(SerialCommands* sender) {
+    redOn = true;
+    sender->GetSerial()->println("Red led is on");
+}
+void cmd_red_off(SerialCommands* sender) {
+    redOn = false;
+    sender->GetSerial()->println("Red led is off");
+}
 //called for ON command
 void cmd_led_on(SerialCommands* sender) {
     pulse_laser = true;
@@ -51,6 +79,12 @@ SerialCommand cmd_led_on_("ON", cmd_led_on);
 SerialCommand cmd_led_off_("OFF", cmd_led_off);
 //SerialCommand cmd_datetime_set_("TS", cmd_datetime_set); // requires one argument
 /// Add one_key commands
+SerialCommand cmd_redo_("R", cmd_red_on, true);
+SerialCommand cmd_redf_("r", cmd_red_off, true);
+SerialCommand cmd_greeno_("G", cmd_green_on, true);
+SerialCommand cmd_greenf_("g", cmd_green_off, true);
+SerialCommand cmd_blueo_("B", cmd_blue_on, true);
+SerialCommand cmd_bluef_("b", cmd_blue_off, true);
 SerialCommand cmd_ledo_("o", cmd_led_on, true);
 SerialCommand cmd_ledf_("f", cmd_led_off, true);
 
@@ -64,6 +98,14 @@ void setup() {
     pinMode(LASER_OUT, INPUT_PULLUP);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
+
+    pinMode(RED_LED, OUTPUT);
+    digitalWrite(RED_LED, HIGH);
+    pinMode(GREEN_LED, OUTPUT);
+    digitalWrite(GREEN_LED, HIGH);
+    pinMode(BLUE_LED, OUTPUT);
+    digitalWrite(BLUE_LED, HIGH);
+
     pinMode(button_pin, INPUT_PULLUP);
     // Start serial port
     Serial.begin(115200);
@@ -72,9 +114,15 @@ void setup() {
     serial_commands_.AddCommand(&cmd_led_off_);
     serial_commands_.AddCommand(&cmd_ledf_);
     serial_commands_.AddCommand(&cmd_ledo_);
+    serial_commands_.AddCommand(&cmd_redf_);
+    serial_commands_.AddCommand(&cmd_redo_);
+    serial_commands_.AddCommand(&cmd_greenf_);
+    serial_commands_.AddCommand(&cmd_greeno_);
+    serial_commands_.AddCommand(&cmd_bluef_);
+    serial_commands_.AddCommand(&cmd_blueo_);
 //    serial_commands_.AddCommand(&cmd_print_);
 
-    Serial.println(F("Ola Mundo!"));
+    Serial.println(F("Ola Mundo! o:pulse ON, f:pulse OFF, R:red ON, r:Red OFF"));
     nextLtime = 0;
     nextBtime = 0;
 }
@@ -82,6 +130,9 @@ void setup() {
 void laser_off() {
     pinMode(LASER_OUT, OUTPUT);
     digitalWrite(LASER_OUT, LOW);
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(BLUE_LED, HIGH);
 }
 
 void laser_pulse(unsigned long now_us) {
@@ -90,6 +141,12 @@ void laser_pulse(unsigned long now_us) {
         if ( now_us > nextLtime ) {
             if(laser_state){
                 pinMode(LASER_OUT, INPUT_PULLUP); // laser ON
+                if(redOn)
+                    digitalWrite(RED_LED, LOW);
+                if(greenOn)
+                    digitalWrite(GREEN_LED, LOW);
+                if(blueOn)
+                    digitalWrite(BLUE_LED, LOW);
             }
             else {
                 laser_off();
